3d3b20fc69c74009e514b0c7df67391416abb6cf
Aman <amanagrawal581@gmail.com>
2023-08-06 21:46:58 +0530

    go live

--- util/database.js
+++ util/database.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD,{
     dialect: 'mysql',
-    host: 'localhost'
+    host: process.env.DB_HOST
 });
 
 module.exports = sequelize;
